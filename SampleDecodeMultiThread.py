d2a39c7de230ae075336e5a5c2022fb3567b167e
Roman Arzumanyan <rarzumanyan@nvidia.com>
2020-06-16 09:38:15 +0300

    changes in sample to illustrate new exception class

--- SampleDecodeMultiThread.py
+++ SampleDecodeMultiThread.py
@@ -18,14 +18,14 @@ import numpy as np
 import sys
 
 from threading import Thread
+
+fout = open('log.txt', 'w')
  
 class Worker(Thread):
-    def __init__(self, gpuID, encFile, outFile):
+    def __init__(self, gpuID, encFile):
         Thread.__init__(self)
 
-        self.decFile = open(outFile, "wb")
-        
-        self.nvDec = nvc.PyNvDecoder(encFile, gpuID)
+        self.nvDec = nvc.PyNvDecoder(encFile, gpuID, {'rtsp_transport': 'tcp', 'max_delay': '5000000', 'bufsize': '30000k'})
         
         width, height = self.nvDec.Width(), self.nvDec.Height()
         hwidth, hheight = int(width / 2), int(height / 2)
@@ -33,64 +33,87 @@ class Worker(Thread):
         self.nvCvt = nvc.PySurfaceConverter(width, height, self.nvDec.Format(), nvc.PixelFormat.YUV420, gpuID)
         self.nvRes = nvc.PySurfaceResizer(hwidth, hheight, self.nvCvt.Format(), gpuID)
         self.nvDwn = nvc.PySurfaceDownloader(hwidth, hheight, self.nvRes.Format(), gpuID)
- 
+        self.num_frame = 0
+
     def run(self):
         try:
             while True:
-                rawSurface = self.nvDec.DecodeSingleSurface()
-                if (rawSurface.Empty()):
-                    print('No more video frames')
-                    break
+                try:
+                    rawSurface = self.nvDec.DecodeSingleSurface()
+                    if (rawSurface.Empty()):
+                        print('No more video frames', file = fout)
+                        fout.flush()
+                        break
+                except nvc.HwResetException:
+                    print('Continue after HW decoder was reset', file = fout)
+                    print('Continue after HW decoder was reset')
+                    fout.flush()
+                    continue
  
                 cvtSurface = self.nvCvt.Execute(rawSurface)
                 if (cvtSurface.Empty()):
-                    print('Failed to do color conversion')
+                    print('Failed to do color conversion', file = fout)
+                    fout.flush()
                     break
 
                 resSurface = self.nvRes.Execute(cvtSurface)
                 if (resSurface.Empty()):
-                    print('Failed to resize surface')
+                    print('Failed to resize surface', file = fout)
+                    fout.flush()
                     break
  
                 rawFrame = np.ndarray(shape=(resSurface.HostSize()), dtype=np.uint8)
                 success = self.nvDwn.DownloadSingleSurface(resSurface, rawFrame)
                 if not (success):
-                    print('Failed to download surface')
+                    print('Failed to download surface', file = fout)
+                    fout.flush()
                     break
  
-                bits = bytearray(rawFrame)
-                self.decFile.write(bits)
+                self.num_frame += 1
+                if( 0 == self.num_frame % self.nvDec.Framerate() ):
+                    print(self.num_frame, file = fout)
+                    fout.flush()
  
         except Exception as e:
             print(getattr(e, 'message', str(e)))
             decFile.close()
  
-def create_threads(gpu_id1, input_file1, output_file1,
-                 gpu_id2, input_file2, output_file2):
+def create_threads(gpu_id1, input_file1):
  
-    th1 = Worker(gpu_id1, input_file1, output_file1)
-    th2 = Worker(gpu_id2, input_file2, output_file2)
+    th1  = Worker(gpu_id1, input_file1)
+    th2  = Worker(gpu_id1, input_file1)
+    th3  = Worker(gpu_id1, input_file1)
+    th4  = Worker(gpu_id1, input_file1)
+    th5  = Worker(gpu_id1, input_file1)
+    th6  = Worker(gpu_id1, input_file1)
+    th7  = Worker(gpu_id1, input_file1)
+    th8  = Worker(gpu_id1, input_file1)
  
     th1.start()
     th2.start()
+    th3.start()
+    th4.start()
+    th5.start()
+    th6.start()
+    th7.start()
+    th8.start()
  
     th1.join()
     th2.join()
+    th3.join()
+    th4.join()
+    th5.join()
+    th6.join()
+    th7.join()
+    th8.join()
  
 if __name__ == "__main__":
-    print("This sample decodes 2 videos simultaneously, resize them to 1/4 of initial size and save to raw YUV files.")
-    print("Usage: SampleDecode.py $gpu_id1 $input_file1 $output_file_1 $gpu_id2 $input_file2 $output_file2")
  
-    if(len(sys.argv) < 7):
+    if(len(sys.argv) < 3):
         print("Provide input CLI arguments as shown above")
         exit(1)
  
     gpu_1 = int(sys.argv[1])
     input_1 = sys.argv[2]
-    output_1 = sys.argv[3]
  
-    gpu_2 = int(sys.argv[4])
-    input_2 = sys.argv[5]
-    output_2 = sys.argv[6]
- 
-    create_threads(gpu_1, input_1, output_1, gpu_2, input_2, output_2)
+    create_threads(gpu_1, input_1)
