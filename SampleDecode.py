c5cd36de7313457ff0621a4ef10c164d8cf4e176
Roman Arzumanyan <rarzumanyan@nvidia.com>
2020-10-13 11:17:35 +0300

    Decode sample modified

--- SampleDecode.py
+++ SampleDecode.py
@@ -19,43 +19,34 @@ import sys
 import av
 
 def decode(gpuID, encFilePath, decFilePath):
-    input_ = av.open(encFilePath)
-    in_stream = input_.streams.video[0]
-    width = in_stream.codec_context.width
-    height = in_stream.codec_context.height
-
-    axbFile = open('AnnexB.h264', "wb")
     decFile = open(decFilePath, "wb")
-    nvDec = nvc.PyNvDecoder(width, height, nvc.PixelFormat.NV12, nvc.CudaVideoCodec.H264, gpuID)
 
-    #Amount of memory in RAM we need to store decoded frame
-    frameSize = int(width * height * 3 / 2)
-    rawFrameNV12 = np.ndarray(shape=(frameSize), dtype=np.uint8)
+    nvDmx = nvc.PyFFmpegDemuxer(encFilePath)
+    nvDec = nvc.PyNvDecoder(nvDmx.Width(), nvDmx.Height(), nvDmx.Format(), nvDmx.Codec(), gpuID)
 
-    for packet in input_.demux(in_stream):
-        if packet.dts is None:
-            continue
+    packet = np.ndarray(shape=(0), dtype=np.uint8)
+    frameSize = int(nvDmx.Width() * nvDmx.Height() * 3 / 2)
+    rawFrame = np.ndarray(shape=(frameSize), dtype=np.uint8)
 
-        packet_data = np.frombuffer(packet.to_bytes(), dtype=np.uint8)
-        bits = bytearray(packet_data)
-        axbFile.write(bits)
+    while True:
+        if not nvDmx.DemuxSinglePacket(packet):
+            break
 
-        success = nvDec.DecodeFrameFromPacket(rawFrameNV12, packet_data)
-        if (success):
-            bits = bytearray(rawFrameNV12)
+        if nvDec.DecodeFrameFromPacket(rawFrame, packet):
+            bits = bytearray(rawFrame)
             decFile.write(bits)
     
 if __name__ == "__main__":
 
-    #print("This sample decodes input video to raw NV12 file on given GPU.")
-    #print("Usage: SampleDecode.py $gpu_id $input_file $output_file.")
+    print("This sample decodes input video to raw NV12 file on given GPU.")
+    print("Usage: SampleDecode.py $gpu_id $input_file $output_file.")
 
-    #if(len(sys.argv) < 4):
-    #    print("Provide gpu ID, path to input and output files")
-    #    exit(1)
+    if(len(sys.argv) < 4):
+        print("Provide gpu ID, path to input and output files")
+        exit(1)
 
-    #gpuID = int(sys.argv[1])
-    #encFilePath = sys.argv[2]
-    #decFilePath = sys.argv[3]
+    gpuID = int(sys.argv[1])
+    encFilePath = sys.argv[2]
+    decFilePath = sys.argv[3]
 
     decode(0, 'big_buck_bunny_1080p_h264.mov', 'bbb.nv12')
