edb7d8fc4721eef490f116b942a6d9e384110617
Roman Arzumanyan <rarzumanyan@nvidia.com>
2021-05-04 17:42:48 +0300

    Fixing samples to load CUDA libraries

--- SampleDecodeSw.py
+++ SampleDecodeSw.py
@@ -14,9 +14,34 @@
 # limitations under the License.
 #
 
+# Starting from Python 3.8 DLL search policy has changed.
+# We need to add path to CUDA DLLs explicitly.
+import sys
+import os
+
+if os.name == 'nt':
+    # Add CUDA_PATH env variable
+    cuda_path = os.environ["CUDA_PATH"]
+    if cuda_path:
+        os.add_dll_directory(cuda_path)
+    else:
+        print("CUDA_PATH environment variable is not set.", file = sys.stderr)
+        print("Can't set CUDA DLLs search path.", file = sys.stderr)
+        exit(1)
+
+    # Add PATH as well for minor CUDA releases
+    sys_path = os.environ["PATH"]
+    if sys_path:
+        paths = sys_path.split(';')
+        for path in paths:
+            if os.path.isdir(path):
+                os.add_dll_directory(path)
+    else:
+        print("PATH environment variable is not set.", file = sys.stderr)
+        exit(1)
+
 import PyNvCodec as nvc
 import numpy as np
-import sys
 
 total_num_frames = 128
 
--- Tests.py
+++ Tests.py
@@ -13,13 +13,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Starting from Python 3.8 DLL search policy has changed.
+# We need to add path to CUDA DLLs explicitly.
+import sys
+import os
+
+if os.name == 'nt':
+    # Add CUDA_PATH env variable
+    cuda_path = os.environ["CUDA_PATH"]
+    if cuda_path:
+        os.add_dll_directory(cuda_path)
+    else:
+        print("CUDA_PATH environment variable is not set.", file = sys.stderr)
+        print("Can't set CUDA DLLs search path.", file = sys.stderr)
+        exit(1)
+
+    # Add PATH as well for minor CUDA releases
+    sys_path = os.environ["PATH"]
+    if sys_path:
+        paths = sys_path.split(';')
+        for path in paths:
+            if os.path.isdir(path):
+                os.add_dll_directory(path)
+    else:
+        print("PATH environment variable is not set.", file = sys.stderr)
+        exit(1)
+
 import SampleDecode as dec
 import SampleEncode as enc
 import PyNvCodec as nvc
-
 import time
-import sys
-import os
  
 if __name__ == "__main__":
 
@@ -44,7 +68,5 @@ if __name__ == "__main__":
     print (str(num_frames), ' frames decoded.')
 
     enc.encode(gpu_id, dec_file, output, 1920, 1080)
-
-    os.remove(dec_file)
 
     exit(0)
