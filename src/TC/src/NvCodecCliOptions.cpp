2e2a7c9983f08d69f31591ee2640c2e0915b503d
rnaskulwar <rnaskulwar@nvidia.com>
2023-04-07 15:18:58 +0530

    setting up correct pixel depth and IDC

--- src/TC/src/NvCodecCliOptions.cpp
+++ src/TC/src/NvCodecCliOptions.cpp
@@ -1300,8 +1300,11 @@ void NvEncoderClInterface::SetupHEVCConfig(NV_ENC_CONFIG_HEVC& config,
     auto pix_fmt = FromString<Pixel_Format>(format);
     if (YUV444 == pix_fmt) {
       config.chromaFormatIDC = 3;
-    } else if (YUV444_10bit == pix_fmt || YUV420_10bit == pix_fmt) {
+    } else if (YUV444_10bit == pix_fmt ) {
       config.chromaFormatIDC = 3;
+      config.pixelBitDepthMinus8 = 2;
+    } else if (YUV420_10bit == pix_fmt) {
+      config.chromaFormatIDC = 1;
       config.pixelBitDepthMinus8 = 2;
     }
   }
