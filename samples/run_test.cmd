a269421f7d3d768ba4f899984244bfd7ab29c82d
rnaskulwar <rnaskulwar@nvidia.com>
2023-04-11 16:21:45 +0530

    correcting pitch alignment

---
+++ samples/run_test.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+
+
+echo running decode tests
+rem python SampleDecode.py -g 0 -e C:\Work\VPF\media\bbb\nv12.h264 -r C:\Work\VPF\media\bbb\nv12.yuv
+rem python SampleDecode.py -g 0 -e C:\Work\VPF\media\bbb\yuv444.h265 -r C:\Work\VPF\media\bbb\yuv444.yuv
+rem python SampleDecode.py -g 0 -e C:\Work\VPF\media\bbb\yuv444p10le.h265 -r C:\Work\VPF\media\bbb\yuv444p10le.yuv
+rem python SampleDecode.py -g 0 -e C:\Work\VPF\media\bbb\yuv420p10le.h265 -r C:\Work\VPF\media\bbb\yuv420p10le.yuv
+
